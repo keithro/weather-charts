fddc6dac60a34a593a3833385cb637bad30b122d
Keith Rodriguez <rodriguez.keith@gmail.com>
2018-03-05 19:01:02 -0500

    Add charts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-// const API_KEY = '000e52245e9cbf2a3bd9ae75ef64c86d'; // my default key
 const API_KEY = '794b32ee432e5c06ccc222f8d0189bdb';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const UNITS = 'imperial';
 
 // To make debugging easier and reduce chances of typo errors we make variables
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`;
+  const url = `${ROOT_URL}&q=${city},us&units=${UNITS}`;
   const request = axios.get(url); // returns promise
   
   return {
